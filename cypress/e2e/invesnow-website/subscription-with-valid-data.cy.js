eeb5a768e22cebf92ed28fd7065d34cbce251300
rinapriani <rina.apriani498@gmail.com>
2022-09-15 15:21:09 +0700

    automation invesnow testing

--- cypress/e2e/invesnow-website/subscription-with-valid-data.cy.js
+++ cypress/e2e/invesnow-website/subscription-with-valid-data.cy.js
@@ -1,5 +1,7 @@
 /// <reference types ="cypress" />
 
+const { get } = require("lodash");
+
 
 describe('Working with input', () => {
     it('visit the website', () => {
@@ -20,17 +22,25 @@ describe('Working with input', () => {
     it('Should choose reksadana', () => {
        
         cy.contains("Reksa Dana").should("have.attr", "href", "/reksadana").click()
-        // cy.url().should('include', '/reksadana', { setTimeout : 10000})
+        cy.url().should('include', '/reksadana', { setTimeout : 10000})
         //cy.wait(15000)
-        cy.intercept('http://test-app.investamart.net/reksadana').as('getReksadana')
+        //cy.intercept('http://test-app.investamart.net/reksadana').as('getReksadana')
 
         // once a request to get settings responds, 'cy.wait' will resolve
-        cy.wait('@getReksadana')
+        //cy.wait('@getReksadana')
 
         //input in search box
         cy.get('#filterInput').type('Semesta Dana Maxima')
 
         cy.contains("Semesta Dana Maxima").click()
         cy.url().should('include', '/subscription-product-detail/MG002MXCSEDAMA00/1772/Semesta%20Dana%20Maxima')
+
+        cy.contains('Beli').should("have.attr", "href", "/subscription/MG002MXCSEDAMA00").click()
+        cy.url().should('include', '/subscription/MG002MXCSEDAMA00', {setTimeout : 10000})
+
+        //input nominal subscription
+        cy.get('#minimum-buy').type('100000')
+        cy.contains("Lanjut").should("have.have.class", "btn mt-2 btn-tertiary py-2 btn-secondary btn-block").click()
+        //cy.get('.btn mt-2 btn-tertiary py-2 btn-secondary btn-block').click()
     });
 });
